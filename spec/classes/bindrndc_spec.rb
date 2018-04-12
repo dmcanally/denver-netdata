410f5929884ed8c3b52a575c35b5698fad91bf84
Denver McAnally <denver@indeed.com>
2018-04-12 10:22:46 -0500

    Fixing bind_rndc plugin job name

--- spec/classes/bindrndc_spec.rb
+++ spec/classes/bindrndc_spec.rb
@@ -45,8 +45,8 @@ describe 'netdata' do
 	it { should contain_concat('/opt/netdata/etc/netdata/python.d/bind_rndc.conf') }
         it { verify_concat_fragment_contents(catalogue, 'bind_rndc.conf+01', /THIS FILE IS MANAGED BY PUPPET/) }
 	it { verify_concat_fragment_exact_contents(catalogue, 'bind_rndc.conf+02_example.com', [
-            'job_name:',
-	    "  name: example.com",
+            "'example.com':",
+	    "  name: 'example.com'",
             "  update_every: 1",
             "  priority: 60000",
             "  retries: 60",
