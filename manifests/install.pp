48af388a23b88bdf3ac2f3015fbed65f30386c38
przemas75 <przemas75@gmail.com>
2019-11-14 21:57:34 +0100

    no bash script with curl (#10)
    *  no bash script with curl
    
    * removed empty line

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,11 +12,11 @@ class netdata::install {
     'curl': {
       exec {'install':
         path    => ['/bin','/sbin','/usr/bin','/usr/sbin'],
-        command => '/bin/bash -c \'bash <(curl -Ss https://my-netdata.io/kickstart-static64.sh) --dont-wait --dont-start-it\'',#lint:ignore:140chars
+        #command => '/bin/bash -c \'bash <(curl -Ss https://my-netdata.io/kickstart-static64.sh) --dont-wait --dont-start-it\'',#lint:ignore:140chars
+        command => 'curl -s https://api.github.com/repos/netdata/netdata/releases/latest | grep -oP \'"tag_name": "\K(.*)(?=")\'| { read ver;curl -Ls https://github.com/netdata/netdata/releases/download/$ver/netdata-$ver.gz.run -o /tmp/netdata-$ver.gz.run; /bin/sh /tmp/netdata-$ver.gz.run --accept --nox11;}',
         unless  => "ls ${install_dir}",
       }
     }
     default: { fail('invalid install_method specified - should be \'curl\' or \'pkg\'') }
   }
-
 }
