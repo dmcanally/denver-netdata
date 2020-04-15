3e6a81b16b1bb4ef34c599ed0ac291854b8bcbd5
denver <denver.mcanally@gmail.com>
2020-04-15 13:54:22 -0500

    Fixing spacing

--- spec/classes/uninstall_spec.rb
+++ spec/classes/uninstall_spec.rb
@@ -5,7 +5,7 @@ describe 'netdata' do
 
     context "on #{os}" do
 
-     let(:facts) do
+      let(:facts) do
         facts
       end
 
@@ -15,15 +15,15 @@ describe 'netdata' do
       })
 
       case facts[:operatingsystem]
-	when 'Ubuntu'
+	      when 'Ubuntu'
           if facts[:operatingsystemmajrelease] <= '14.10'
-	    supports_uninstall = false
+	          supports_uninstall = false
           else
-	    supports_uninstall = true
+	          supports_uninstall = true
           end
-      else
-	  supports_uninstall = true
-      end
+        else
+	        supports_uninstall = true
+        end
 
       case facts[:operatingsystemmajrelease]
         when /^(14|6)/
@@ -32,30 +32,29 @@ describe 'netdata' do
           service_file = '/etc/systemd/system/netdata.service'
       end
 
-      let(:params) do
-        {
-	  :ensure => 'absent',
+      let(:params) do {
+	      :ensure => 'absent',
         }.merge(overridden_params)
       end
       if supports_uninstall == true
         describe "uninstall netdata on #{os}" do
-          let(:overridden_params) do {
-          } end
+          let(:overridden_params) do {}
+          end
   
           it { should compile.with_all_deps }
           it { is_expected.to contain_class('netdata::uninstall') }
-  	  it { is_expected.to contain_file('/etc/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/opt/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/usr/sbin/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/usr/share/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/usr/libexec/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/var/lib/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/var/cache/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/var/log/netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file("#{service_file}").with('ensure' => 'absent') }
-  	  it { is_expected.to contain_file('/etc/cron.daily/netdata-updater.sh').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_user('netdata').with('ensure' => 'absent') }
-  	  it { is_expected.to contain_group('netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/etc/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/opt/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/usr/sbin/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/usr/share/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/usr/libexec/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/var/lib/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/var/cache/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/var/log/netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file("#{service_file}").with('ensure' => 'absent') }
+  	      it { is_expected.to contain_file('/etc/cron.daily/netdata-updater.sh').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_user('netdata').with('ensure' => 'absent') }
+  	      it { is_expected.to contain_group('netdata').with('ensure' => 'absent') }
         end
       end
     end
